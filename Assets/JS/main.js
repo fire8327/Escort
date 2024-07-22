a980d2d4605a2d69c4dce8785e02ae013a1bd51d
fire8327 <fire83274@gmail.com>
2024-07-22 03:59:21 +0300

    create hero slider and main container

---
+++ Assets/JS/main.js
@@ -0,0 +1,23 @@
+/* hero-slider */
+const swiper = new Swiper('.hero-slider', {
+    loop: true,
+    navigation: {
+      nextEl: '.hero-next',
+      prevEl: '.hero-prev',
+    },
+    spaceBetween: 10,
+    breakpoints: {
+        320: {
+          slidesPerView: 1.5,
+        },
+        768: {
+          slidesPerView: 4,
+        },
+        1024: {
+          slidesPerView: 5,
+        },
+        1280: {
+          slidesPerView: 7,
+        },
+    }
+});
